a60bd35178167368620e58a87a08bb93a48fea6d
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-09-07 00:54:52 +0200

    Add bad word check

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function getRudeWords(cb) {
-  $.ajax({
-    url: '/badwords-en.txt',
-    success: function(result) {
-      cb(null, result.split('\n'));
-    },
-    cb
+$(document).ready(function() {
+  // responsiveVoice.speak('You were a rude rude boy!');
+  $('#play').click(function() {
+    console.log('???');
+  });
+
+  $('#submit-button').click(function() {
+    let text = $('#input').val();
+    submitReply(text);
+  });
+});
+
+function getRudeWordsSet(cb) {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: '/badwords-en.txt',
+      success: function(result) {
+        let set = new Set();
+
+        for (let s of result.split('\n')) {
+          set.add(s);
+        }
+
+        resolve(set);
+      },
+      reject
+    });
   });
 }
 
-getRudeWords(function(err, words) {
-  console.log('??', err, words);
-});
+async function textContainsBadWord(text) {
+  let words = await getRudeWordsSet();
+  for (let word of text.split(/\s/)) {
+    if (words.has(word)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function submitReply(text) {
+  let hasBadWord = await textContainsBadWord(text);
+  console.log('has bad word', hasBadWord);
+  if (hasBadWord) {
+    responsiveVoice.speak('You were a rude rude boy!');
+  }
+}
